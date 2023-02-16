086028f988633bc2d63dc5bf21f4bb0f83c25d48
chanlaw <chanlaw@berkeley.edu>
2023-02-15 20:35:43 -0800

    add nn training

--- mi_scratch.py
+++ mi_scratch.py
@@ -8,6 +8,8 @@ import plotly.express as px  # type: ignore
 import scipy
 from sklearn.metrics import mutual_info_score  # type: ignore
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torchvision import datasets, transforms
 
 # %%
@@ -201,5 +203,83 @@ ax.plot(mis)
 ax.set_title("Mutual information after greedily removing pixels")
 ax.set_xlabel("Number of pixels removed")
 ax.set_ylabel(f"Mutual information between image and X[{index[0]},{index[1]}]")
+
+# %%
+# train a small conv net on the MNIST dataset
+# %%
+# load the MNIST dataset
+train_dataset = datasets.MNIST(
+    root="data", train=True, transform=transforms.ToTensor(), download=True
+)
+test_dataset = datasets.MNIST(root="data", train=False, transform=transforms.ToTensor())
+# %%
+# create the data loaders
+batch_size = 128
+train_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True
+)
+test_loader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=batch_size, shuffle=False
+)
+# %%
+# define the model
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(7 * 7 * 64, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = x.reshape(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+# %%
+# initialize the model
+model = ConvNet()
+# %%
+# define the optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.99))
+# %%
+# define the loss function
+criterion = nn.CrossEntropyLoss()
+# %%
+# train the model
+num_epochs = 10
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # backward pass and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print(
+                f"Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}"
+            )
+    # report test loss, accuracy
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        print(
+            f"Test Accuracy of the model on the 10000 test images: {correct / total * 100:.2f} %"
+        )
 
 # %%
